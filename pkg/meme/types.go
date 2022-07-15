a8e2359c63f42289f77b501caf4c0406c0d7d2b4
Adam MacDonald <adam.macdonald@spoton.com>
2022-07-15 17:15:08 -0400

    feat: create postgres tables

--- pkg/meme/types.go
+++ pkg/meme/types.go
@@ -1,5 +1,9 @@
 package meme
 
+import (
+	"time"
+)
+
 type RepositoryType string
 
 const (
@@ -11,14 +15,20 @@ type Meme struct {
 	// ID is a unique identifier for the meme.
 	ID string `json:"id"`
 
-	// ImgPath is the path to the image.
-	ImgPath string `json:"path"`
-
 	// TemplateID defines the template that was used to create this meme.
 	TemplateID string `json:"templateId"`
 
+	// ImgPath is the path to the image. This will be a partial path that is missing a base URL (e.g. assets/my-img.png).
+	ImgPath string `json:"path"`
+
 	// Text defines the text used in the meme. Stored here for future indexing and searching.
 	Text []string `json:"text"`
+
+	// UserID defines the user who created the meme.
+	UserID string `json:"userId"`
+
+	// CreatedOn defines the date/time the meme was created.
+	CreatedOn time.Time `json:"createdOn"`
 }
 
 // CreateMemeFromTemplate informs the Service on how to create a meme.
